df2abf81590e01896ac98c1822d3109747addb6a
Thomas Lahore <tom.lahore@gmail.com>
2024-12-25 08:08:27 -0800

    undo kinda sorta working with thread lock

--- app/decorators.py
+++ app/decorators.py
@@ -4,7 +4,10 @@ from .models.api_transaction import ApiTransaction
 from .core.config import ENABLE_UNDO_REDO
 from functools import wraps
 from sqlalchemy.orm import Session
+from threading import Lock
 
+# Add a lock for transaction management
+transaction_lock = Lock()
 
 def db_transaction_decorator(func):
     @wraps(func)
@@ -27,43 +30,50 @@ def api_transaction_decorator(func):
             print(f"Undo/redo is disabled, skipping transaction for function: {func.__name__}")
             return func(*args, **kwargs)
 
-        # # Start a new transaction
-        if global_state["current_transaction"] is not None:
-            raise Exception("Transaction already in progress")
-        global_state["current_transaction"] = ApiTransaction()
-        print(f"@ Starting transaction for function: {func.__name__}")
-        try:
-            #####################################################
-            # Execute the wrapped function
-            result = func(*args, **kwargs)
-            print(f"@ Function {func.__name__} executed successfully")
-            #####################################################
+        with transaction_lock:
+            if global_state["current_transaction"] is not None:
+                raise Exception("Transaction already in progress")
+            global_state["current_transaction"] = ApiTransaction()
+            print(f"@ Starting transaction for function: {func.__name__}")
 
-            print('@@@ notes before updated:')
-            for k in global_state["current_transaction"].state_before_updated.keys():
-                print(f'\t{k[:8]}')
-            print('@@@ notes current updated:')
-            for k in global_state["current_transaction"].state_current_updated.keys():
-                print(f'\t{k[:8]}')
-            print('@@@ notes added:')
-            for k in global_state["current_transaction"].state_added.keys():
-                print(f'\t{k[:8]}')
-            print('@@@ notes deleted:')
-            for k in global_state["current_transaction"].state_deleted.keys():
-                print(f'\t{k[:8]}')
+            try:
+                #####################################################
+                # Execute the wrapped function
+                result = func(*args, **kwargs)
+                print(f"@ Function {func.__name__} executed successfully")
+                #####################################################
 
-            # # Finalize the transaction
-            # # TODO asdfasdf is this source of problem?
-            # # global_state["current_transaction"].finalize_transaction()
-            # # # Clear the transaction after use
-            global_state["current_transaction"] = None
-            # # # this is an action so gets rid of prior redos
-            # # command_stack = global_state["command_stack"]
-            # # command_stack.clear_after_current()
-            # print(f"@ Transaction ended for function: {func.__name__}")
-            return result
-        except Exception as e:
-            print(f"Exception occurred in function {func.__name__}: {e}")
-            raise e
+                print('@@@ notes before updated:')
+                for k in global_state["current_transaction"].state_before_updated.keys():
+                    val = global_state["current_transaction"].state_before_updated[k].content[:8]
+                    print(f'\t{k[:8]} content="{val}"')
+                print('@@@ notes current updated:')
+                for k in global_state["current_transaction"].state_current_updated.keys():
+                    val = global_state["current_transaction"].state_current_updated[k].content[:8]
+                    print(f'\t{k[:8]} content="{val}"')
+                print('@@@ notes added:')
+                for k in global_state["current_transaction"].state_added.keys():
+                    val = global_state["current_transaction"].state_added[k].content[:8]
+                    print(f'\t{k[:8]} content="{val}"')
+                print('@@@ notes deleted:')
+                for k in global_state["current_transaction"].state_deleted.keys():
+                    val = global_state["current_transaction"].state_deleted[k].content[:8]
+                    print(f'\t{k[:8]} content="{val}"')
+
+                # # Finalize the transaction
+                global_state["current_transaction"].finalize_transaction()
+                # Clear the transaction after use
+                global_state["current_transaction"] = None
+                # this is an action so gets rid of prior redos
+                command_stack = global_state["command_stack"]
+                # command_stack.clear_after_current()
+                print(f"@ Transaction ended for function: {func.__name__}")
+
+                print(f'command stack size: {len(command_stack.stack)}')
+
+                return result
+            except Exception as e:
+                print(f"Exception occurred in function {func.__name__}: {e}")
+                raise e
 
     return wrapper
--- app/global_state.py
+++ app/global_state.py
@@ -1,7 +1,9 @@
 from .undo_redo import CommandStack
+from threading import Lock
 
 # A simple dictionary to hold global state
 global_state = {
     "current_transaction": None,
-    "command_stack": CommandStack()
+    "command_stack": CommandStack(),
+    "lock": Lock()
 } 
