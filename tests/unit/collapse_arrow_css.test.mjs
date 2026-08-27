57be6d4572a812abeb55a4c8018c18ccbb3732fa
Thomas Lahore <tom.lahore@gmail.com>
2026-08-26 18:50:16 -0700

    Enlarge note collapse controls

---
+++ tests/unit/collapse_arrow_css.test.mjs
@@ -0,0 +1,14 @@
+import assert from 'node:assert/strict';
+import { readFile } from 'node:fs/promises';
+import test from 'node:test';
+
+const MAIN_CSS_URL = new URL('../../app/static/css/main.css', import.meta.url);
+
+test('collapse arrow uses a larger glyph and centered hit area', async () => {
+    const cssSource = await readFile(MAIN_CSS_URL, 'utf8');
+
+    assert.match(
+        cssSource,
+        /\.note-collapse-toggle\s*\{[^}]*top:\s*3px;[^}]*left:\s*-29px;[^}]*width:\s*27px;[^}]*height:\s*27px;[^}]*font-size:\s*23px;/s,
+    );
+});
